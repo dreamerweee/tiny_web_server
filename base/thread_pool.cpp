2b4d0ad702a02426b19ae5aaf5db77e189c8c08f
weee <wtdreamer@163.com>
2020-05-04 16:33:02 +0800

    thread_pool

--- base/thread_pool.cpp
+++ base/thread_pool.cpp
@@ -1,7 +1,35 @@
 #include "thread_pool.h"
+#include "log.h"
+
+void* ThreadPool::Worker(void *arg)
+{
+    ThreadPool *pool = static_cast<ThreadPool *>(arg);
+    pool->Run();
+    return pool;
+}
+
+void ThreadPool::Run()
+{
+    while (!m_stop) {
+        MutexGuard lock(m_mutex);
+        while (m_task_queue.empty()) {
+            m_cond.Wait();
+        }
+        if (m_stop) {
+            LOG_INFO("thread id[0x%x] exit\n", (unsigned)pthread_self());
+            pthread_exit(NULL);
+        }
+        ThreadTask *task = m_task_queue.front();
+        m_task_queue.pop();
+        if (!task) {
+            continue;
+        }
+        task->cb_func(task->cb_arg);
+    }
+}
 
 ThreadPool::ThreadPool(int thread_cnt) :
-    m_thread_count(thread_cnt)
+    m_thread_count(thread_cnt), m_stop(false)
 {
     assert(thread_cnt > 0);
     m_threads = new pthread_t[m_thread_count];
@@ -9,5 +37,6 @@ ThreadPool::ThreadPool(int thread_cnt) :
     // 创建线程
     for (int i = 0; i < m_thread_count; ++i) {
         assert(pthread_create(m_threads + i, NULL, Worker, this) == 0);
+        assert(pthread_detach(m_threads[i]) == 0);
     }
 }
--- base/thread_pool.h
+++ base/thread_pool.h
@@ -4,15 +4,22 @@
 #include "locker.h"
 #include <queue>
 
+typedef void (*TaskCallback) (void *arg);
+
+struct ThreadTask {
+    TaskCallback cb_func;
+    void *cb_arg;
+};
+
 class ThreadPool {
 public:
     ThreadPool(int thread_cnt = 5);
     ~ThreadPool();
 
-    void AppendWork(void *request);
+    void AppendWork(void *task);
 
 private:
-    static void* Worker(void *args);
+    static void* Worker(void *arg);
     void Run();
 
 private:
@@ -20,7 +27,7 @@ private:
     int m_thread_count;   // 线程数量
     pthread_t *m_threads; // 线程数组
 
-    std::queue<void *> m_work_queue;  // 工作队列
+    std::queue<ThreadTask *> m_task_queue;  // 工作队列
     Mutex m_mutex;     // 保护工作队列的互斥锁
     Condition m_cond;  // 工作队列为空时等待的条件变量
 }
