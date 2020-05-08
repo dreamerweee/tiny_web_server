d90774bb8c503a79a9005cb64fab0f7e49275207
weee <wtdreamer@163.com>
2020-05-08 19:21:32 +0800

    thread_pool

--- base/thread_pool.cpp
+++ base/thread_pool.cpp
@@ -19,12 +19,13 @@ void ThreadPool::Run()
             LOG_INFO("thread id[0x%x] exit\n", (unsigned)pthread_self());
             pthread_exit(NULL);
         }
-        ThreadTask *task = m_task_queue.front();
+        Task *task = m_task_queue.front();
         m_task_queue.pop();
         if (!task) {
             continue;
         }
         task->cb_func(task->cb_arg);
+        delete task;
     }
 }
 
@@ -32,11 +33,34 @@ ThreadPool::ThreadPool(int thread_cnt) :
     m_thread_count(thread_cnt), m_stop(false)
 {
     assert(thread_cnt > 0);
-    m_threads = new pthread_t[m_thread_count];
-    assert(m_threads != NULL);
     // 创建线程
     for (int i = 0; i < m_thread_count; ++i) {
-        assert(pthread_create(m_threads + i, NULL, Worker, this) == 0);
-        assert(pthread_detach(m_threads[i]) == 0);
+        pthread_t *p_thread = new pthread_t;
+        m_threads.push_back(p_thread);
+        assert(pthread_create(p_thread, NULL, Worker, this) == 0);
+        assert(pthread_detach(*p_thread) == 0);
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    for (int i = 0; i < m_thread_count; ++i) {
+        delete m_threads[i];
+    }
+    if (!m_task_queue.empty()) {
+        delete m_task_queue.front();
+        m_task_queue.pop();
+    }
+    m_stop = true;
+}
+
+void ThreadPool::AppendWork(TaskCallback func, void *arg)
+{
+    Task *task = new Task(func, arg);
+    MutexGuard lock(m_mutex);
+    bool empty_flag = m_task_queue.empty();
+    m_task_queue.push(task);
+    if (empty_flag) {
+        m_cond.Signal();
     }
 }
--- base/thread_pool.h
+++ base/thread_pool.h
@@ -1,22 +1,17 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
-#include "locker.h"
+#include <vector>
 #include <queue>
-
-typedef void (*TaskCallback) (void *arg);
-
-struct ThreadTask {
-    TaskCallback cb_func;
-    void *cb_arg;
-};
+#include "locker.h"
+#include "task.h"
 
 class ThreadPool {
 public:
     ThreadPool(int thread_cnt = 5);
     ~ThreadPool();
 
-    void AppendWork(void *task);
+    void AppendWork(TaskCallback func, void *arg);
 
 private:
     static void* Worker(void *arg);
@@ -25,9 +20,9 @@ private:
 private:
     bool m_stop;
     int m_thread_count;   // 线程数量
-    pthread_t *m_threads; // 线程数组
+    std::vector<pthread_t *> m_threads;  // 线程数组
 
-    std::queue<ThreadTask *> m_task_queue;  // 工作队列
+    std::queue<Task *> m_task_queue;  // 工作队列
     Mutex m_mutex;     // 保护工作队列的互斥锁
     Condition m_cond;  // 工作队列为空时等待的条件变量
 }
